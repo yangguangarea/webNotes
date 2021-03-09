b9a1aff32ab1b00f51da2608a35e76d1095d5d90
yangguang22 <yangguang22@58.com>
2021-03-09 19:00:04 +0800

    10-对象原型

--- webRecord/test/test10.js
+++ webRecord/test/test10.js
@@ -1,13 +1,21 @@
-class Father {
-    constructor(x) {
-        this.x = x;
-    }
-}
+    // 1.利用 new Object() 创建对象
+    var obj1 = new Object();
 
-class Son extends Father {
-    constructor(x) {
-        this.x = x;
-        super(x + 1);
+    // 2.利用对象字面量创建对象
+    var obj2 = {};
+
+    // 3.利用构造函数创建对象
+    function ObjFun(name, age) {
+        this.name = name;
+        this.age = age;
+        this.printName = function() {
+            console.log(this.name);
+        }
     }
-}
-let son = new Son(1);
+    ObjFun.prototype.printAge = function() {
+        console.log(this.age);
+    }
+
+    var obj3 = new ObjFun('name1', 23);
+    obj3.printName();
+    obj3.printAge();
